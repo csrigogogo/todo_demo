777ea6f3d6ca9db412f7212c506f8c89737c01d1
crisri-mac <crifsri@icloud.com>
2023-02-02 16:34:02 +0800

    init

---
+++ src/Wrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout style="width: auto;">
+    <a-layout-header>Header</a-layout-header>
+    <a-layout-content style="margin: 0 10%; min-width: 1000px;">
+      <App />
+    </a-layout-content>
+    <a-layout-footer>Footer</a-layout-footer>
+  </a-layout>
+</template>
+
+<script setup>
+import App from "./App.vue";
+</script>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ArcoResolver } from "unplugin-vue-components/resolvers";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ArcoResolver()],
+    }),
+    Components({
+      resolvers: [
+        ArcoResolver({
+          sideEffect: true,
+        }),
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    outDir: "../test_mogenius/front_dist",
+  },
+});
